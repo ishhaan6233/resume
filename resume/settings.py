40af67f908fd8abe5bf7c1f8297bf98ddb91c44d
Ishhaan6233 <imeena@ualberta.ca>
2025-09-13 09:47:19 -0600

    Fixed filepath in home.html added settings for css

--- resume/settings.py
+++ resume/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     "resume_app",
 ]
 
+STATIC_URL = '/static/'
+
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
