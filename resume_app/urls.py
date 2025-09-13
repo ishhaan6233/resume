bd5b71ec6bc03c19219d089a99507c812994adaa
Ishhaan6233 <imeena@ualberta.ca>
2025-09-13 16:11:20 -0600

    delete fixed

--- resume_app/urls.py
+++ resume_app/urls.py
@@ -5,11 +5,7 @@ urlpatterns = [
     path("", views.home, name="home"),
     path("applications/", views.applications_view, name="applications"),
     path("resumes/", views.resumes, name="resumes"),
-<<<<<<< HEAD
-    path("applications/add/", views.add_application, name="add_application")
-=======
     path("responses/", views.responses, name="responses"),
     path("applications/add/", views.add_application, name="add_application"),
-    path("resumes/<int:resume_id>/download/", views.download_resume_pdf, name="download_resume_pdf"),
->>>>>>> 16cbdabdbe94f08da41e65a84a03d3eac51b9ef0
+    path("applications/delete/<int:app_id>/", views.delete_application, name="delete_application"),
 ]
