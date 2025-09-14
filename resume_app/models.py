2295d14956826fbadc8f0441db49d8edb091f71f
Ishhaan6233 <imeena@ualberta.ca>
2025-09-14 12:37:05 -0600

    my changes

--- resume_app/models.py
+++ resume_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+
 class Response(models.Model):
     company = models.CharField(max_length=255)
     position = models.CharField(max_length=255)
